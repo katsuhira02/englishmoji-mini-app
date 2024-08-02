c6fa5d602ce6db19f590feec54c79944afe79385
danil <feldanil2007@gmail.com>
2024-08-02 19:01:03 +0500

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState, useEffect, ReactNode } from 'react';
-import bridge, { UserInfo } from '@vkontakte/vk-bridge';
 import { View, SplitLayout, SplitCol, ScreenSpinner } from '@vkontakte/vkui';
 import { useActiveVkuiLocation } from '@vkontakte/vk-mini-apps-router';
 
@@ -8,19 +7,15 @@ import { DEFAULT_VIEW_PANELS } from './routes';
 
 export const App = () => {
   const { panel: activePanel = DEFAULT_VIEW_PANELS.TEST } = useActiveVkuiLocation();
-  const [fetchedUser, setUser] = useState<UserInfo | undefined>();
   const [popout, setPopout] = useState<ReactNode | null>(<ScreenSpinner size="large" />);
 
   useEffect(() => {
     async function fetchData() {
-      const user = await bridge.send('VKWebAppGetUserInfo');
-      setUser(user);
       setPopout(null);
     }
     fetchData();
   }, []);
 
-  console.log(fetchedUser, "fetchedUser");
   return (
     <SplitLayout popout={popout}>
       <SplitCol>
